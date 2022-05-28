e5c30127db738e18673de59787b88d2777e09bc4
“Derwin <derwin1310@gmail.com>
2022-05-28 19:55:42 -0400

    webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,74 +1,54 @@
-const HtmlWebpack = require('html-webpack-plugin');
-const MiniCssExtract = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const CssMinimizer = require('css-minimizer-webpack-plugin');
-const Terser = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
 
-module.exports = {
-    mode: 'production',
+/** @type {import('webpack').Configuration} */
+const prodConfig = {
+	mode: 'production',
+	devtool: 'source-map',
+	module: {
+		rules: [
+			{
+				test: '/.(scss|sass)$/',
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '../../',
+						},
+					},
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: '/.css$/',
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: '../../',
+						},
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			name: false,
+		},
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'static/css/[name].[contenthash:8].css',
+			chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+		}),
+	],
+};
 
-    output: {
-        clean: true,
-        filename: 'main.[contenthash].js'
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-                options: {
-                    sources: false
-                }
-            },
-            {
-                test: /\.css$/i,
-                exclude: '/styles.css$/',
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: '/styles.css$/',
-                use: [MiniCssExtract.loader, 'css-loader']
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-            }
-        ]
-    },
-
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizer(),
-            new Terser()
-        ]
-    },
-
-    plugins: [
-        new HtmlWebpack({
-            template: '/src/index.html'
-        }),
-
-        new MiniCssExtract({
-            filename: '[name].[fullhash].css',
-            ignoreOrder: false
-        }),
-
-        new CopyPlugin({
-            patterns: [
-                { from: 'src/assets/', to: 'assets/' }
-            ]
-        })
-    ]
-}
+module.exports = merge(common, prodConfig);
